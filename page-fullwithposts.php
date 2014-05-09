1ca56f119a793295a157a34ab7ab9e59714b8863
Tim Nicholson <tim@xtremelysocial.com>
2014-05-09 08:56:20 -0500

    Update page with posts
    Update page-fullwithposts.php to put the page meta and comment section
    after the recent posts. This looks much nicer.

--- page-fullwithposts.php
+++ page-fullwithposts.php
@@ -6,10 +6,10 @@
  *
  * Full width page template (no sidebar, no container) with 3 columns of recent posts
  *
- * This is the template for full width pages with no sidebar and
- * no container. It also lists your 3 most recent posts. This page truly stretches 
- * the full width of the browser window. You should set a <div class="container"> 
- * before your content to keep it in line with the rest of the site content.
+ * This is the template for full width pages with no sidebar and no container. It also
+ * lists your 3 most recent posts. This page truly stretches the full width of the 
+ * browser window. You should set a <div class="container"> before your content to keep 
+ * it in line with the rest of the site content.
  *
  * @package flat-bootstrap
  */
@@ -21,10 +21,26 @@ get_header(); ?>
 <div id="primary" class="content-area-wide">
 	<main id="main" class="site-main" role="main">
 	
-		<?php /* DISPLAY THE PAGE CONTENT FIRST IF THERE IS ANY */ ?>
+		<?php /* DISPLAY THE PAGE CONTENT FIRST */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'page-fullwidth' ); ?>
+			<?php //get_template_part( 'content', 'page-fullwidth' ); ?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<div class="entry-content">
+
+					<?php the_content(); ?>
+		
+					<?php get_template_part( 'content', 'page-nav' ); ?>
+
+					<?php get_template_part( 'content', 'recent-posts' ); ?>
+
+					<?php edit_post_link( __( '<span class="glyphicon glyphicon-edit"></span> Edit', 'flat-bootstrap' ), '<div class="container"><footer class="entry-meta"><div class="edit-link">', '</div></div></footer>' ); ?>
+
+				</div><!-- .entry-content -->
+	
+			</article><!-- #post-## -->
 
 			<?php
 			// If comments are open or we have at least one comment, load up the comment template
@@ -38,49 +54,6 @@ get_header(); ?>
 			<?php endif; ?>
 
 		<?php endwhile; // end of the loop. ?>
-
-		<?php /* DISPLAY THE MOST RECENT POSTS (NOTE STICKY POSTS FIRST) */
-		$num_posts = 3; // Should be a factor of 12 column grid
-		$paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
-		$args = array(
-			'post_type' => 'post',
-			'posts_per_page' => $num_posts,
-			'paged' => $paged
-		);
-		$list_of_posts = new WP_Query( $args );
-		?>
-		<?php if ( $list_of_posts->have_posts() ) : ?>
-			<div id="page-posts" class="page-posts">
-			<div class="container"><div class="row">
-
-			<?php /* Determine # of columns and # of posts per row */
-			//$count_posts = count ( $list_of_posts->posts );
-			$count_posts = count ( $list_of_posts->posts ) <= $num_posts ? count ( $list_of_posts->posts ) : $num_posts;
-			if ( $count_posts % 4 == 0 ) $per_row = 4;
-			elseif ( $count_posts % 3 == 0) $per_row = 3;
-			else $per_row = 2;			
-			$num_cols = 12 / $per_row;
-			?>
-
-			<?php /* The loop */ ?>
-			<?php $count = 0; ?>
-			<?php while ( $list_of_posts->have_posts() AND $count < $num_posts ) : $list_of_posts->the_post(); ?>
-				<?php if ( $count > 0 AND $count % $per_row == 0 ) echo '</div><div class="row">'; ?>
-				<div class="col-lg-<?php echo $num_cols ?>">
-				<?php // Display content of posts ?>
-				<?php get_template_part( 'content', 'page-posts' ); ?>
-				</div>
-				<?php $count++; ?>
-			<?php endwhile; ?>
-
-			</div><!-- row --></div><!-- container -->
-			</div><!-- page-posts -->
-
-		<?php endif; ?>	
-		<?php 
-		/* Restore original Post Data */
-		wp_reset_postdata();		
-		?>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
